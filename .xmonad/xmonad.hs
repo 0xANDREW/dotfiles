ccda69c680a6e41589097de8884c57486105a847
Andrew Muro <andrew.t.muro@gmail.com>
2015-10-26 13:10:49 -0400

    changed spotify bindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -37,44 +37,11 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
  
-    -- -- Move focus to the next window
-    -- , ((modm,               xK_j     ), windows W.focusDown)
- 
-    -- -- Move focus to the previous window
-    -- , ((modm,               xK_k     ), windows W.focusUp  )
- 
-    -- -- Move focus to the master window
-    -- , ((modm,               xK_m     ), windows W.focusMaster  )
- 
     -- Swap the focused window and the master window
     , ((modm,               xK_Return), windows W.swapMaster)
  
-    -- -- Swap the focused window with the next window
-    -- , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
- 
-    -- -- Swap the focused window with the previous window
-    -- , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
- 
-    -- -- Shrink the master area
-    -- , ((modm,               xK_h     ), sendMessage Shrink)
- 
-    -- -- Expand the master area
-    -- , ((modm,               xK_l     ), sendMessage Expand)
- 
     -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
- 
-    -- -- Increment the number of windows in the master area
-    -- , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
- 
-    -- -- Deincrement the number of windows in the master area
-    -- , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
- 
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
  
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
@@ -83,13 +50,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
 
     , ((modm .|. shiftMask, xK_x     ), spawn "xscreensaver-command -lock")
-
-    -- , ((modm, xK_Left), spawn "spotify-control.py prev")
-    -- , ((modm, xK_Right), spawn "spotify-control.py next")
-    -- , ((modm, xK_Up), spawn "spotify-control.py play_pause")
-    -- , ((modm, xK_Down), spawn "spotify-control.py stop")
-    -- , ((modm, xK_o), spawn "spotify-control.py full_info")
-    -- , ((modm .|. shiftMask, xK_d), spawn "set-display.py")
 
     , ((modm, xK_1), windows (viewOnScreen 1 "www/1"))
     , ((modm, xK_2), windows (viewOnScreen 0 "dev/2"))
@@ -101,26 +61,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm, xK_8), windows (viewOnScreen 0 "m2/8"))
     , ((modm, xK_9), windows (viewOnScreen 0 "m3/9"))
     ]
-    -- ++
- 
-    -- --
-    -- -- mod-[1..9], Switch to workspace N
-    -- --
-    -- -- mod-[1..9], Switch to workspace N
-    -- -- mod-shift-[1..9], Move client to workspace N
-    -- --
-    -- [((m .|. modm, k), windows $ f i)
-    --     | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-    --     , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    -- ++
-
-    
-    -- -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    
-    -- [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-    --     | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-    --     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 wksp = [ "www/1", "dev/2", "dev2/3", "vbox/4", "pidgin/5", "music/6", "m1/7", "m2/8", "m3/9" ]
 
@@ -168,11 +108,6 @@ manage_hook = composeAll [
   className =? "Skype" --> doFloat
 
   ] <+> manageDocks
-
-
--- startup_hook = do
---   spawn "/home/andrew/bin/trayer.sh"
-  -- setWMName "LG3D"
   
 main = do
   xmproc <- spawnPipe "xmobar"
@@ -187,7 +122,6 @@ main = do
       keys              = myKeys,
       manageHook        = manage_hook,
       layoutHook        = layout_hook,
-      -- startupHook       = startup_hook,
       logHook           = dynamicLogWithPP xmobarPP {
         ppOutput = hPutStrLn xmproc,
         ppTitle = xmobarColor "green" "" . shorten 50,
@@ -196,8 +130,7 @@ main = do
       }
     `additionalKeysP`
     [
-      ("<XF86AudioNext>", spawn "spotify-control.py next"),
-      ("<XF86AudioPrev>", spawn "spotify-control.py prev"),
-      ("<XF86AudioStop>", spawn "spotify-control.py stop"),
-      ("<XF86AudioPlay>", spawn "spotify-control.py play_pause")
+      ("<XF86AudioNext>", spawn "playerctl --player=spotify next"),
+      ("<XF86AudioPrev>", spawn "playerctl --player=spotify previous"),
+      ("<XF86AudioPlay>", spawn "playerctl --player=spotify play-pause")
     ]

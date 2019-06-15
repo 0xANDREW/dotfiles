b33dbbc3aa67dfdc4574faa6cf4aaa8a9e03d3c7
andy <andrew@tankutility.com>
2019-06-15 09:33:12 -0700

    updates

--- polybar/darksky.sh
+++ polybar/darksky.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+source $HOME/bin/no-commit-secrets.sh
+
 LATLNG="34.2116,-118.2322"
 KEY=$DARKSKY_API_KEY
 
 JSON=`curl https://api.darksky.net/forecast/$KEY/$LATLNG?units=si 2> /dev/null`
-
 TEMP=`echo $JSON | jq '.currently.temperature'`
 SUMM=`echo $JSON | jq -r '.currently.summary'`
 
